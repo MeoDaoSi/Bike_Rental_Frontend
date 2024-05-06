6eb571a11bb5755d97055029914538210715973f
Ám Vương <102019281+MeoDaoSi@users.noreply.github.com>
2024-05-06 17:03:52 +0700

    fix deploy netlify

--- bike-rental/src/main.tsx
+++ bike-rental/src/main.tsx
@@ -3,10 +3,39 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { MessengerChat } from "react-messenger-chat-plugin";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <>
+    <MessengerChat
+      pageId="334884689697859"
+      language="sv_SE"
+      themeColor={"#000000"}
+      bottomSpacing={300}
+      loggedInGreeting="loggedInGreeting"
+      loggedOutGreeting="loggedOutGreeting"
+      greetingDialogDisplay={"show"}
+      debugMode={true}
+      onMessengerShow={() => {
+        console.log("onMessengerShow");
+      }}
+      onMessengerHide={() => {
+        console.log("onMessengerHide");
+      }}
+      onMessengerDialogShow={() => {
+        console.log("onMessengerDialogShow");
+      }}
+      onMessengerDialogHide={() => {
+        console.log("onMessengerDialogHide");
+      }}
+      onMessengerMounted={() => {
+        console.log("onMessengerMounted");
+      }}
+      onMessengerLoad={() => {
+        console.log("onMessengerLoad");
+      }}
+    />
     <ToastContainer />
     <App />
   </>
